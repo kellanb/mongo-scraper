49be1bcd4ad29c4071a649b55ce64ff8dd86126f
kellb <k_benn24@hotmail.com>
2019-04-19 23:55:15 -0500

    pasted files from my local project folder into github repo

---
+++ models/Article.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
